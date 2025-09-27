4d41a8d03a621240810ff7d4600f67a87a9c899d
technikyvl <jakub4.biznes@gmail.com>
2025-09-27 16:02:36 +0200

    animacja spedu-p

--- MAGICLIFELANDING/app/page.jsx
+++ MAGICLIFELANDING/app/page.jsx
@@ -376,8 +376,8 @@ export default function Page() {
         }
       });
     }, { 
-      threshold: 0.1,
-      rootMargin: "0px 0px -50px 0px"
+      threshold: 0.05,
+      rootMargin: "0px 0px 50px 0px"
     });
 
     // Observe all reveal elements
--- MAGICLIFELANDING/app/szkolenia/page.jsx
+++ MAGICLIFELANDING/app/szkolenia/page.jsx
@@ -20,8 +20,8 @@ export default function TrainingPage() {
         }
       });
     }, { 
-      threshold: 0.1,
-      rootMargin: "0px 0px -50px 0px"
+      threshold: 0.05,
+      rootMargin: "0px 0px 50px 0px"
     });
 
     // Observe all reveal elements
